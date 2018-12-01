1c2969b34771ad3397a57b456755e56d66a327f2
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-12-01 15:34:59 -0500

    Add benchmarks links to template

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,5 +1,11 @@
 //! # Day XX:
 //!
+//! [Benchmarking report](../../../dayXX/target/criterion/report/index.html):
+//!
+//! * [Part 1](../../../dayXX/target/criterion/dayXX_part1/report/index.html)
+//! * [Part 2](../../../dayXX/target/criterion/dayXX_part2/report/index.html)
+//!
+//!
 //! ## Part One
 //!
 //!
