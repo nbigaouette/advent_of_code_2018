acd34b1de077e9217350faf5235a0ac46588ba9a
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-12-02 09:24:40 -0500

    Solution to day 2

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -17,13 +17,18 @@
 
 #[macro_use]
 extern crate log;
-
-use std::fmt::Debug;
+extern crate strsim;
+#[macro_use]
+extern crate itertools;
 
 use std::collections::HashMap;
+use std::fmt::Debug;
+
+use strsim::hamming;
 
 pub trait AoC<'a>: Debug {
-    type Solution;
+    type Solution1;
+    type Solution2;
     type Data;
 
     fn description(&self) -> &'static str {
@@ -36,32 +41,38 @@ pub trait AoC<'a>: Debug {
 
     fn parsed(&self) -> Self::Data;
 
-    fn solution_part1(&self) -> Self::Solution {
+    fn solution_part1(&self) -> Self::Solution1 {
         unimplemented!()
     }
 
-    fn solution_part2(&self) -> Self::Solution {
+    fn solution_part2(&self) -> Self::Solution2 {
         unimplemented!()
     }
 }
 
-type Day02Solution = i64;
-type Day02Data<'a> = Box<Iterator<Item = Day02Solution> + 'a>;
+type Day02SolutionPart1 = i64;
+type Day02SolutionPart2 = String;
+type Day02Data<'a> = Box<Iterator<Item = Day02SolutionPart1> + 'a>;
 
-static PUZZLE_INPUT: &str = include_str!("../input");
+pub static PUZZLE_INPUT: &str = include_str!("../input");
 
 pub mod benchmark {
     use super::*;
 
     pub type ToBenchmark<'a> = Day02BuildIter<'a>;
-    pub type BenchmarkVector<'a> =
-        Vec<Box<dyn AoC<'a, Solution = Day02Solution, Data = Day02Data<'a>> + 'a>>;
+    pub type BenchmarkVector<'a> = Vec<
+        Box<
+            dyn AoC<
+                    'a,
+                    Solution1 = Day02SolutionPart1,
+                    Solution2 = Day02SolutionPart2,
+                    Data = Day02Data<'a>,
+                > + 'a,
+        >,
+    >;
 
     pub fn to_benchmark<'a>() -> BenchmarkVector<'a> {
-        unimplemented!()
-        // vec![
-        //     Box::new(Day02BuildIter::new(PUZZLE_INPUT)),
-        // ]
+        vec![Box::new(Day02BuildIter::new(PUZZLE_INPUT))]
     }
 }
 
@@ -71,7 +82,8 @@ pub struct Day02BuildIter<'a> {
 }
 
 impl<'a> AoC<'a> for Day02BuildIter<'a> {
-    type Solution = Day02Solution;
+    type Solution1 = Day02SolutionPart1;
+    type Solution2 = Day02SolutionPart2;
     type Data = Day02Data<'a>;
 
     fn description(&self) -> &'static str {
@@ -87,51 +99,62 @@ impl<'a> AoC<'a> for Day02BuildIter<'a> {
         unimplemented!()
     }
 
-    fn solution_part1(&self) -> Self::Solution {
+    fn solution_part1(&self) -> Self::Solution1 {
         let mut count_two = 0;
         let mut count_three = 0;
         self.input.lines().for_each(|line| {
             let line = line.trim();
-            println!(
-                "line: {}   count_two: {}   count_three: {}",
-                line, count_two, count_three
-            );
             let mut seen = HashMap::new();
             let mut line_count_two = 0;
             let mut line_count_three = 0;
             line.chars().for_each(|c| {
                 let n = seen.entry(c).or_insert(0);
                 *n += 1;
-                println!("    c: {}  n: {}", c, n);
                 if *n == 2 {
                     line_count_two += 1;
-                    println!(
-                        "        --> line_count_two +=1 --> {}  line_count_three: {}",
-                        line_count_two, line_count_three
-                    );
                 } else if *n == 3 {
                     line_count_two -= 1;
                     line_count_three += 1;
-                    println!(
-                        "        --> line_count_two -=1 --> {}  line_count_three +=1 --> {}",
-                        line_count_two, line_count_three
-                    );
                 }
             });
             count_two += line_count_two.min(1);
             count_three += line_count_three.min(1);
-
-            println!(
-                "  --> count_two: {}   count_three: {}",
-                count_two, count_three
-            );
         });
 
         count_two * count_three
     }
 
-    // fn solution_part2(&self) -> Self::Solution {
-    // }
+    fn solution_part2(&self) -> Self::Solution2 {
+        let mut max_same_chars = 0;
+        #[derive(Debug)]
+        struct CommonLines<'a> {
+            line1: &'a str,
+            line2: &'a str,
+        }
+        let mut matched_lines = None;
+        let lines: Vec<_> = self.input.lines().map(|line| line.trim()).collect();
+        for i in 0..lines.len() {
+            for j in i + 1..lines.len() {
+                let distance = hamming(lines[i], lines[j]).unwrap();
+                let same_chars = lines[i].len() - distance;
+                if same_chars > max_same_chars {
+                    max_same_chars = same_chars;
+                    matched_lines = Some(CommonLines {
+                        line1: lines[i],
+                        line2: lines[j],
+                    });
+                }
+            }
+        }
+        let matched_lines = matched_lines.unwrap();
+        let same_chars: String = matched_lines
+            .line1
+            .chars()
+            .zip(matched_lines.line2.chars())
+            .filter_map(|(c1, c2)| if c1 == c2 { Some(c1) } else { None })
+            .collect();
+        same_chars
+    }
 }
 
 #[cfg(test)]
@@ -153,8 +176,6 @@ mod tests {
 
     mod aoc2018 {
         mod day02 {
-            const PUZZLE_INPUT: &'static str = include_str!("../input");
-
             mod part1 {
 
                 mod solution {
@@ -211,9 +232,7 @@ mod tests {
                     fn solution() {
                         init_logger();
 
-                        unimplemented!();
-
-                        let expected = 0;
+                        let expected = "ymdrchgpvwfloluktajxijsqb";
                         let to_check = Day02BuildIter::new(PUZZLE_INPUT).solution_part2();
 
                         assert_eq!(expected, to_check);
@@ -228,10 +247,14 @@ mod tests {
                     fn ex01() {
                         init_logger();
 
-                        unimplemented!();
-
-                        let expected = 0;
-                        let input = "";
+                        let expected = "fgij";
+                        let input = "abcde
+                                     fghij
+                                     klmno
+                                     pqrst
+                                     fguij
+                                     axcye
+                                     wvxyz";
                         let to_check = Day02BuildIter::new(input).solution_part2();
 
                         assert_eq!(expected, to_check);
