519016edc02487036d1cc6fe9d6cfd247e1f31d8
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-12-01 22:54:57 -0500

    Replace spaces with underscore in benchmark name since used in paths

--- day01/benches/aoc_benchmark.rs
+++ day01/benches/aoc_benchmark.rs
@@ -12,9 +12,10 @@ fn criterion_benchmark_part1(c: &mut Criterion) {
     let functions: Vec<_> = to_benchmark()
         .into_iter()
         .map(|s| {
-            Fun::new(s.description(), move |b: &mut Bencher, _: &()| {
-                b.iter(|| s.solution_part1())
-            })
+            Fun::new(
+                &s.description().replace(" ", "_"),
+                move |b: &mut Bencher, _: &()| b.iter(|| s.solution_part1()),
+            )
         }).collect();
 
     c.bench_functions("day01_part1", functions, ());
@@ -24,9 +25,10 @@ fn criterion_benchmark_part2(c: &mut Criterion) {
     let functions: Vec<_> = to_benchmark()
         .into_iter()
         .map(|s| {
-            Fun::new(s.description(), move |b: &mut Bencher, _: &()| {
-                b.iter(|| s.solution_part2())
-            })
+            Fun::new(
+                &s.description().replace(" ", "_"),
+                move |b: &mut Bencher, _: &()| b.iter(|| s.solution_part2()),
+            )
         }).collect();
 
     c.bench_functions("day01_part2", functions, ());
