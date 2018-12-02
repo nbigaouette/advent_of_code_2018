cdf702e11cfe42c47412c818b1aaf9baa29787a7
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-12-01 22:53:32 -0500

    Move solution implementation to own module

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -88,8 +88,10 @@
 // #[macro_use]
 // extern crate log;
 
-use std::collections::HashMap;
 use std::fmt::Debug;
+
+mod dynparse;
+pub use dynparse::Day01BuildIter;
 
 pub trait AoC<'a>: Debug {
     type Solution;
@@ -129,52 +131,6 @@ fn parse_input<'a>(input: &'a str) -> impl Iterator<Item = i64> + 'a {
 type Day01Solution = i64;
 type Day01Data<'a> = Box<Iterator<Item = Day01Solution> + 'a>;
 
-#[derive(Debug)]
-pub struct Day01BuildIter<'a> {
-    input: &'a str,
-}
-
-impl<'a> AoC<'a> for Day01BuildIter<'a> {
-    type Solution = Day01Solution;
-    type Data = Day01Data<'a>;
-
-    fn description(&self) -> &'static str {
-        "Parse string dynamically"
-    }
-
-    fn new(input: &'a str) -> Day01BuildIter {
-        Day01BuildIter { input }
-    }
-
-    fn parsed(&self) -> Self::Data {
-        Box::new(parse_input(self.input))
-    }
-
-    fn solution_part1(&self) -> Self::Solution {
-        self.parsed().sum()
-    }
-
-    fn solution_part2(&self) -> Self::Solution {
-        let mut seen_frequencies = HashMap::new();
-        let mut frequency = 0;
-
-        // Insert initial point
-        seen_frequencies.insert(frequency, 1);
-
-        let inputs: Vec<_> = self.parsed().collect();
-
-        while inputs.iter().cycle().find(|&&i| {
-            frequency += i;
-            let freq_count = seen_frequencies.entry(frequency).or_insert(0);
-            *freq_count += 1;
-            *freq_count == 2
-        }) == None
-        {}
-
-        frequency
-    }
-}
-
 static PUZZLE_INPUT: &str = include_str!("../input");
 
 pub mod benchmark {
@@ -185,7 +141,11 @@ pub mod benchmark {
         Vec<Box<dyn AoC<'a, Solution = Day01Solution, Data = Day01Data<'a>> + 'a>>;
 
     pub fn to_benchmark<'a>() -> BenchmarkVector<'a> {
-        vec![Box::new(Day01BuildIter::new(PUZZLE_INPUT))]
+        vec![
+            Box::new(Day01BuildIter::new(PUZZLE_INPUT)),
+            Box::new(Day01PreParseHashMap::new(PUZZLE_INPUT)),
+            Box::new(Day01PreParseHashSet::new(PUZZLE_INPUT)),
+        ]
     }
 }
 
@@ -194,7 +154,9 @@ mod tests {
     extern crate env_logger;
     use std::env;
 
-    fn init_logger() {
+    use crate::parse_input;
+
+    pub fn init_logger() {
         env::var("RUST_LOG")
             .or_else(|_| -> Result<String, ()> {
                 let rust_log = "debug".to_string();
@@ -206,161 +168,9 @@ mod tests {
         let _ = env_logger::try_init();
     }
 
-    mod aoc2018 {
-        mod day01 {
-            use crate::{AoC, Day01BuildIter};
-
-            #[test]
-            fn parse() {
-                let parsed: Vec<_> = Day01BuildIter::new("+1, -2, +3, +1").parsed().collect();
-                assert_eq!(parsed, vec![1, -2, 3, 1]);
-            }
-
-            mod part1 {
-
-                mod solution {
-                    use crate::tests::init_logger;
-                    use crate::{AoC, Day01BuildIter, PUZZLE_INPUT};
-
-                    #[test]
-                    fn solution() {
-                        init_logger();
-
-                        let expected = 408;
-                        let to_check = Day01BuildIter::new(PUZZLE_INPUT).solution_part1();
-
-                        assert_eq!(expected, to_check);
-                    }
-                }
-
-                mod given {
-                    use crate::tests::init_logger;
-                    use crate::{AoC, Day01BuildIter};
-
-                    #[test]
-                    fn ex01() {
-                        init_logger();
-
-                        let expected = 3;
-                        let input = "+1, -2, +3, +1";
-                        let to_check = Day01BuildIter::new(input).solution_part1();
-
-                        assert_eq!(expected, to_check);
-                    }
-
-                    #[test]
-                    fn ex02() {
-                        init_logger();
-
-                        let expected = 3;
-                        let input = "+1, +1, +1";
-                        let to_check = Day01BuildIter::new(input).solution_part1();
-
-                        assert_eq!(expected, to_check);
-                    }
-
-                    #[test]
-                    fn ex03() {
-                        init_logger();
-
-                        let expected = 0;
-                        let input = "+1, +1, -2";
-                        let to_check = Day01BuildIter::new(input).solution_part1();
-
-                        assert_eq!(expected, to_check);
-                    }
-
-                    #[test]
-                    fn ex04() {
-                        init_logger();
-
-                        let expected = -6;
-                        let input = "-1, -2, -3";
-                        let to_check = Day01BuildIter::new(input).solution_part1();
-
-                        assert_eq!(expected, to_check);
-                    }
-                }
-
-                /*
-                mod extra {
-                    use ::*;
-                }
-                */
-            }
-
-            mod part2 {
-
-                mod solution {
-                    use crate::tests::init_logger;
-                    use crate::{AoC, Day01BuildIter, PUZZLE_INPUT};
-
-                    #[test]
-                    fn solution() {
-                        init_logger();
-
-                        let expected = 55250;
-                        let to_check = Day01BuildIter::new(PUZZLE_INPUT).solution_part2();
-
-                        assert_eq!(expected, to_check);
-                    }
-                }
-
-                mod given {
-                    use crate::tests::init_logger;
-                    use crate::{AoC, Day01BuildIter};
-
-                    #[test]
-                    fn ex01() {
-                        init_logger();
-
-                        let expected = 0;
-                        let input = "+1, -1";
-                        let to_check = Day01BuildIter::new(input).solution_part2();
-
-                        assert_eq!(expected, to_check);
-                    }
-
-                    #[test]
-                    fn ex02() {
-                        init_logger();
-
-                        let expected = 10;
-                        let input = "+3, +3, +4, -2, -4";
-                        let to_check = Day01BuildIter::new(input).solution_part2();
-
-                        assert_eq!(expected, to_check);
-                    }
-
-                    #[test]
-                    fn ex03() {
-                        init_logger();
-
-                        let expected = 5;
-                        let input = "-6, +3, +8, +5, -6";
-                        let to_check = Day01BuildIter::new(input).solution_part2();
-
-                        assert_eq!(expected, to_check);
-                    }
-
-                    #[test]
-                    fn ex04() {
-                        init_logger();
-
-                        let expected = 14;
-                        let input = "+7, +7, -2, -7, -4";
-                        let to_check = Day01BuildIter::new(input).solution_part2();
-
-                        assert_eq!(expected, to_check);
-                    }
-                }
-
-                /*
-                mod extra {
-                    use ::*;
-                }
-                */
-            }
-        }
+    #[test]
+    fn parse() {
+        let parsed: Vec<_> = parse_input("+1, -2, +3, +1").collect();
+        assert_eq!(parsed, vec![1, -2, 3, 1]);
     }
 }
