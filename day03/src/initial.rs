b79069d49f19b9ba7852ce33604059be91184cf3
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-12-03 14:07:14 -0500

    Solution to day 3 part 1

--- day03/src/initial.rs
+++ day03/src/initial.rs
@@ -1,67 +1,117 @@
-use crate::{parse_input, AoC, DayXXParsed, DayXXSolutionPart1, DayXXSolutionPart2};
+use std::collections::HashMap;
 
-#[derive(Debug)]
-pub struct DayXXInitial<'a> {
-    input: &'a str,
+use crate::{parse_input, AoC, Day03Parsed, Day03SolutionPart1, Day03SolutionPart2, Input};
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+struct Coord {
+    i: u64,
+    j: u64,
 }
 
-impl<'a> AoC<'a> for DayXXInitial<'a> {
-    type SolutionPart1 = DayXXSolutionPart1;
-    type SolutionPart2 = DayXXSolutionPart2;
-    type Parsed = DayXXParsed<'a>;
+#[derive(Debug)]
+pub struct Day03Initial<'a> {
+    input: &'a str,
+
+    seen: HashMap<Coord, u64>,
+    // seen: HashMap<Coord, Vec<u64>>,
+}
+
+impl<'a> AoC<'a> for Day03Initial<'a> {
+    type SolutionPart1 = Day03SolutionPart1;
+    type SolutionPart2 = Day03SolutionPart2;
+    type Parsed = Day03Parsed<'a>;
 
     fn description(&self) -> &'static str {
         "Parse string dynamically"
     }
 
-    fn new(input: &'a str) -> DayXXInitial {
-        DayXXInitial { input }
+    fn new(input: &'a str) -> Day03Initial {
+        Day03Initial {
+            input,
+            seen: HashMap::new(),
+        }
     }
 
     // fn parsed(&self) -> Self::Parsed {
     //     Box::new(parse_input(self.input))
     // }
 
-    // fn solution_part1(&self) -> Self::SolutionPart1 {
-    // }
+    fn solution_part1(&mut self) -> Self::SolutionPart1 {
+        let mut count = 0;
+        for claim in parse_input(self.input).map(|i| Input::from(i)) {
+            for i in claim.left..(claim.left + claim.wide) {
+                for j in claim.top..(claim.top + claim.tall) {
+                    let coord = Coord { i, j };
+                    let point = self.seen.entry(coord).or_insert(0);
+                    *point += 1;
+                    if *point == 2 {
+                        count += 1;
+                        // println!(
+                        //     "Coord: {:?}  point: {}  count: {}",
+                        //     Coord { i, j },
+                        //     point,
+                        //     count
+                        // );
+                    }
+                    // let point = self.seen.entry(coord).or_insert(vec![]);
+                    // point.push(claim.id);
+                    // if point.len() == 2 {
+                    //     count += 1;
+                    // }
+                }
+            }
+        }
+        count
+    }
 
-    // fn solution_part2(&self) -> Self::SolutionPart2 {
-    // }
+    fn solution_part2(&mut self) -> Self::SolutionPart2 {
+        // Calculate the hashmap
+        // let _ = self.solution_part1();
+        unimplemented!()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     mod part1 {
         mod solution {
-            use super::super::super::DayXXInitial;
+            use super::super::super::Day03Initial;
             use crate::{tests::init_logger, AoC, PUZZLE_INPUT};
+
+            #[test]
+            fn bugged_solution_01() {
+                init_logger();
+
+                let not_expected = 128626;
+                let to_check = Day03Initial::new(PUZZLE_INPUT).solution_part1();
+
+                assert_ne!(not_expected, to_check);
+            }
 
             #[test]
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
-                let to_check = DayXXInitial::new(PUZZLE_INPUT).solution_part1();
+                let expected = 100595;
+                let to_check = Day03Initial::new(PUZZLE_INPUT).solution_part1();
 
                 assert_eq!(expected, to_check);
             }
         }
 
         mod given {
-            use super::super::super::DayXXInitial;
+            use super::super::super::Day03Initial;
             use crate::{tests::init_logger, AoC};
 
             #[test]
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
-                let input = "";
-                let to_check = DayXXInitial::new(input).solution_part1();
+                let expected = 4;
+                let input = "#1 @ 1,3: 4x4
+                             #2 @ 3,1: 4x4
+                             #3 @ 5,5: 2x2";
+                let to_check = Day03Initial::new(input).solution_part1();
 
                 assert_eq!(expected, to_check);
             }
@@ -76,35 +126,33 @@ mod tests {
 
     mod part2 {
         mod solution {
-            use super::super::super::DayXXInitial;
+            use super::super::super::Day03Initial;
             use crate::{tests::init_logger, AoC, PUZZLE_INPUT};
 
             #[test]
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
                 let expected = 0;
-                let to_check = DayXXInitial::new(PUZZLE_INPUT).solution_part2();
+                let to_check = Day03Initial::new(PUZZLE_INPUT).solution_part2();
 
                 assert_eq!(expected, to_check);
             }
         }
 
         mod given {
-            use super::super::super::DayXXInitial;
+            use super::super::super::Day03Initial;
             use crate::{tests::init_logger, AoC};
 
             #[test]
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
-                let input = "";
-                let to_check = DayXXInitial::new(input).solution_part2();
+                let expected = 3;
+                let input = "#1 @ 1,3: 4x4
+                             #2 @ 3,1: 4x4
+                             #3 @ 5,5: 2x2";
+                let to_check = Day03Initial::new(input).solution_part2();
 
                 assert_eq!(expected, to_check);
             }
--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -15,10 +15,13 @@
 //!
 //!
 
-#[macro_use]
-extern crate log;
+// #[macro_use]
+// extern crate log;
 
 use std::fmt::Debug;
+
+pub mod initial;
+pub use initial::Day03Initial;
 
 pub trait AoC<'a>: Debug {
     type SolutionPart1;
@@ -37,23 +40,43 @@ pub trait AoC<'a>: Debug {
         unimplemented!()
     }
 
-    fn solution_part1(&self) -> Self::SolutionPart1 {
+    fn solution_part1(&mut self) -> Self::SolutionPart1 {
         unimplemented!()
     }
 
-    fn solution_part2(&self) -> Self::SolutionPart2 {
+    fn solution_part2(&mut self) -> Self::SolutionPart2 {
         unimplemented!()
     }
 }
 
-fn parse_input<'a>(input: &'a str) -> impl Iterator<Item = i64> + 'a {
-    unimplemented!();
-    vec![].into_iter()
+pub fn parse_input<'a>(input: &'a str) -> impl Iterator<Item = InputStr<'a>> + 'a {
+    input.lines().map(move |line| {
+        let mut s = line.trim().split(' ');
+        // Id
+        let id = &s.next().unwrap()[1..];
+        // Skip @
+        let _ = s.next().unwrap();
+        // Position
+        let mut position = s.next().unwrap().split(',');
+        let left = position.next().unwrap();
+        let top = position.next().unwrap().trim_end_matches(':');
+        // Size
+        let mut size = s.next().unwrap().split('x');
+        let wide = size.next().unwrap();
+        let tall = size.next().unwrap();
+        InputStr {
+            id,
+            left,
+            top,
+            wide,
+            tall,
+        }
+    })
 }
 
 type Day03SolutionPart1 = i64;
 type Day03SolutionPart2 = i64;
-type Day03Data<'a> = Box<Iterator<Item = Day03SolutionPart1> + 'a>;
+type Day03Parsed<'a> = Box<Iterator<Item = Day03SolutionPart1> + 'a>;
 
 pub static PUZZLE_INPUT: &str = include_str!("../input");
 
@@ -66,16 +89,43 @@ pub mod benchmark {
                     'a,
                     SolutionPart1 = Day03SolutionPart1,
                     SolutionPart2 = Day03SolutionPart2,
-                    Parsed = Day03Data<'a>,
+                    Parsed = Day03Parsed<'a>,
                 > + 'a,
         >,
     >;
 
     pub fn to_benchmark<'a>() -> BenchmarkVector<'a> {
-        unimplemented!()
-        // vec![
-        //     Box::new(Day03BuildIter::new(PUZZLE_INPUT)),
-        // ]
+        vec![Box::new(Day03Initial::new(PUZZLE_INPUT))]
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct InputStr<'a> {
+    id: &'a str,
+    left: &'a str,
+    top: &'a str,
+    wide: &'a str,
+    tall: &'a str,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Input {
+    id: u64,
+    left: u64,
+    top: u64,
+    wide: u64,
+    tall: u64,
+}
+
+impl<'a> From<InputStr<'a>> for Input {
+    fn from(input: InputStr<'a>) -> Self {
+        Input {
+            id: input.id.parse().unwrap(),
+            left: input.left.parse().unwrap(),
+            top: input.top.parse().unwrap(),
+            wide: input.wide.parse().unwrap(),
+            tall: input.tall.parse().unwrap(),
+        }
     }
 }
 
@@ -85,6 +135,7 @@ mod tests {
     use std::env;
 
     use crate::parse_input;
+    use crate::{Input, InputStr};
 
     pub fn init_logger() {
         env::var("RUST_LOG")
@@ -99,9 +150,74 @@ mod tests {
     }
 
     #[test]
+    fn parse_ex01() {
+        init_logger();
+
+        let input = "#123 @ 3,2: 5x4";
+        let parsed: Vec<InputStr> = parse_input(input).collect();
+        assert_eq!(
+            parsed,
+            vec![InputStr {
+                id: "123",
+                left: "3",
+                top: "2",
+                wide: "5",
+                tall: "4",
+            }]
+        );
+    }
+
+    #[test]
+    fn parse_ex02() {
+        init_logger();
+
+        let input = "#1 @ 1,3: 4x4
+                     #2 @ 3,1: 4x4
+                     #3 @ 5,5: 2x2";
+        let parsed: Vec<InputStr> = parse_input(input).collect();
+        assert_eq!(
+            parsed,
+            vec![
+                InputStr {
+                    id: "1",
+                    left: "1",
+                    top: "3",
+                    wide: "4",
+                    tall: "4",
+                },
+                InputStr {
+                    id: "2",
+                    left: "3",
+                    top: "1",
+                    wide: "4",
+                    tall: "4",
+                },
+                InputStr {
+                    id: "3",
+                    left: "5",
+                    top: "5",
+                    wide: "2",
+                    tall: "2",
+                },
+            ]
+        );
+    }
+
+    #[test]
     fn parse() {
-        unimplemented!();
-        let parsed: Vec<_> = parse_input("").collect();
-        assert_eq!(parsed, vec![]);
+        init_logger();
+
+        let input = "#123 @ 3,2: 5x4";
+        let parsed: Vec<Input> = parse_input(input).map(|i| Input::from(i)).collect();
+        assert_eq!(
+            parsed,
+            vec![Input {
+                id: 123,
+                left: 3,
+                top: 2,
+                wide: 5,
+                tall: 4,
+            }]
+        );
     }
 }
