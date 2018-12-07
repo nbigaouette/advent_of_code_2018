96bb0f44653534cd4d82762b9642d946477c2b53
Nicolas Bigaouette <nbigaouette@gmail.com>
2018-12-06 22:18:49 -0500

    Solution to day 6

--- day06/src/initial.rs
+++ day06/src/initial.rs
@@ -1,4 +1,12 @@
+use std::collections::HashSet;
+
+use itertools::iproduct;
+use ndarray::Array2;
+
+use crate::{manhattan_distance, Position, Result};
 use crate::{parse_input, AoC, Day06SolutionPart1, Day06SolutionPart2};
+
+const SAFE_CELL_MARKER: CoordinateId = CoordinateId::max_value();
 
 #[derive(Debug)]
 pub struct Day06Initial<'a> {
@@ -17,11 +25,222 @@ impl<'a> AoC<'a> for Day06Initial<'a> {
         Day06Initial { input }
     }
 
-    // fn solution_part1(&self) -> Self::SolutionPart1 {
-    // }
+    fn solution_part1(&self) -> Self::SolutionPart1 {
+        solution_part1(self.input).unwrap()
+    }
 
-    // fn solution_part2(&self) -> Self::SolutionPart2 {
-    // }
+    fn solution_part2(&self) -> Self::SolutionPart2 {
+        solution_part2(self.input, 10000).unwrap()
+    }
+}
+
+pub fn solution_part1(input: &str) -> Result<Day06SolutionPart1> {
+    // Find the size of the circle surrounding _all_ points.
+    // This will be the max iteration since the two most distant
+    // point's growing neighborhood will have reach the other point.
+    let positions = parse_input(&input)?;
+    let bounding_box = find_bounding_box(&positions)?;
+    let (mut grid, grid_offset) = new_grid(&bounding_box);
+
+    for (id, pos) in positions.iter().enumerate() {
+        grid[pos.ij(&grid_offset)] = Cell::C(id as CoordinateId);
+    }
+
+    // Fill grid
+    fill_grid_part1(&mut grid, &bounding_box, &positions);
+
+    let ids_at_boundary = boundaries(&grid);
+
+    let max_count = positions
+        .iter()
+        .enumerate()
+        .filter(|(id, _pos)| !ids_at_boundary.contains(&(*id as u32)))
+        .map(|(id, _pos)| {
+            grid.iter()
+                .filter(move |cell| match cell {
+                    Cell::C(cell_id) => cell_id == &(id as u32),
+                    _ => false,
+                })
+                .count()
+        })
+        .max_by_key(|count| *count)
+        .unwrap();
+
+    Ok(max_count as i64)
+}
+
+pub fn solution_part2(input: &str, max_distance: i64) -> Result<Day06SolutionPart2> {
+    // Find the size of the circle surrounding _all_ points.
+    // This will be the max iteration since the two most distant
+    // point's growing neighborhood will have reach the other point.
+    let positions = parse_input(&input)?;
+    let bounding_box = find_bounding_box(&positions)?;
+    let (mut grid, grid_offset) = new_grid(&bounding_box);
+
+    for (id, pos) in positions.iter().enumerate() {
+        grid[pos.ij(&grid_offset)] = Cell::C(id as CoordinateId);
+    }
+
+    // Fill grid
+    fill_grid_part2(&mut grid, &bounding_box, &positions, max_distance);
+
+    Ok(grid
+        .iter()
+        .filter(|&cell| *cell == Cell::C(SAFE_CELL_MARKER))
+        .count() as Day06SolutionPart2)
+}
+
+pub type CoordinateId = u32;
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum Cell {
+    U___,
+    C(CoordinateId),
+    T___,
+}
+pub type Grid = Array2<Cell>;
+
+pub fn boundaries(grid: &Grid) -> HashSet<CoordinateId> {
+    let row_first: Vec<_> = grid.slice(s![0, ..]).into_iter().collect();
+    let row_last: Vec<_> = grid.slice(s![-1, ..]).into_iter().collect();
+    let col_first: Vec<_> = grid.slice(s![.., 0]).into_iter().collect();
+    let col_last: Vec<_> = grid.slice(s![.., -1]).into_iter().collect();
+    let boundary_elements_dup = [row_first, row_last, col_first, col_last].concat();
+    let mut boundary_elements = HashSet::new();
+    for element in boundary_elements_dup {
+        match element {
+            Cell::C(id) => {
+                boundary_elements.insert(*id);
+            }
+            _ => { /* */ }
+        }
+    }
+    boundary_elements
+}
+
+fn fill_grid_part1(grid: &mut Grid, bounding_box: &BoundingBox, positions: &[Position]) {
+    let mut closest_id: Vec<CoordinateId> = Vec::with_capacity(64);
+    for (j_i64, i_i64) in iproduct!(0..bounding_box.height(), 0..bounding_box.width()) {
+        let i = i_i64 as usize;
+        let j = j_i64 as usize;
+
+        closest_id.clear();
+        let mut min_dist = i64::max_value();
+
+        let cell = &mut grid[[j, i]];
+
+        let cell_pos = Position {
+            x: i_i64 + bounding_box.xmin,
+            y: j_i64 + bounding_box.ymin,
+        };
+        if *cell == Cell::U___ {
+            for (id, pos) in positions.iter().enumerate() {
+                let distance = manhattan_distance(pos, &cell_pos);
+                if min_dist == distance {
+                    closest_id.push(id as u32);
+                } else if distance < min_dist {
+                    closest_id.clear();
+                    closest_id.push(id as u32);
+                    min_dist = distance;
+                }
+            }
+            assert!(!closest_id.is_empty());
+
+            if closest_id.len() == 1 {
+                *cell = Cell::C(closest_id[0]);
+            } else {
+                *cell = Cell::T___;
+            }
+        }
+    }
+}
+
+fn fill_grid_part2(
+    grid: &mut Grid,
+    bounding_box: &BoundingBox,
+    positions: &[Position],
+    max_distance_sum: i64,
+) {
+    for (j_i64, i_i64) in iproduct!(0..bounding_box.height(), 0..bounding_box.width()) {
+        let i = i_i64 as usize;
+        let j = j_i64 as usize;
+
+        let cell = &mut grid[[j, i]];
+
+        let cell_pos = Position {
+            x: i_i64 + bounding_box.xmin,
+            y: j_i64 + bounding_box.ymin,
+        };
+        let distances_sum: i64 = positions
+            .iter()
+            .map(|pos| manhattan_distance(pos, &cell_pos))
+            .sum();
+        if distances_sum < max_distance_sum {
+            *cell = Cell::C(SAFE_CELL_MARKER);
+        }
+    }
+}
+
+pub fn new_grid(bounding_box: &BoundingBox) -> (Grid, [i64; 2]) {
+    let grid_offset = [bounding_box.xmin, bounding_box.ymin];
+
+    (
+        Grid::from_elem(
+            (
+                bounding_box.height() as usize,
+                bounding_box.width() as usize,
+            ),
+            Cell::U___,
+        ),
+        grid_offset,
+    )
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BoundingBox {
+    xmin: i64,
+    xmax: i64,
+    ymin: i64,
+    ymax: i64,
+}
+
+impl BoundingBox {
+    pub fn width(&self) -> i64 {
+        self.xmax - self.xmin + 1
+    }
+    pub fn height(&self) -> i64 {
+        self.ymax - self.ymin + 1
+    }
+}
+
+fn find_bounding_box(input: &[Position]) -> Result<BoundingBox> {
+    let xmin = input
+        .iter()
+        .min_by_key(|pos| pos.x)
+        .ok_or_else(|| format!("Can't find minimum x value"))?
+        .x;
+    let xmax = input
+        .iter()
+        .max_by_key(|pos| pos.x)
+        .ok_or_else(|| format!("Can't find maximum x value"))?
+        .x;
+
+    let ymin = input
+        .iter()
+        .min_by_key(|pos| pos.y)
+        .ok_or_else(|| format!("Can't find minimum y value"))?
+        .y;
+    let ymax = input
+        .iter()
+        .max_by_key(|pos| pos.y)
+        .ok_or_else(|| format!("Can't find maximum y value"))?
+        .y;
+
+    Ok(BoundingBox {
+        xmin,
+        xmax,
+        ymin,
+        ymax,
+    })
 }
 
 #[cfg(test)]
@@ -35,9 +254,7 @@ mod tests {
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
+                let expected = 3290;
                 let to_check = Day06Initial::new(PUZZLE_INPUT).solution_part1();
 
                 assert_eq!(to_check, expected);
@@ -52,22 +269,252 @@ mod tests {
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
-                let input = "";
+                let expected = 17;
+                let input = "1, 1
+                             1, 6
+                             8, 3
+                             3, 4
+                             5, 5
+                             8, 9";
                 let to_check = Day06Initial::new(input).solution_part1();
 
                 assert_eq!(to_check, expected);
             }
         }
 
-        /*
         mod extra {
-            use super::super::super::Day06Initial;
-            use crate::{tests::init_logger, AoC, PUZZLE_INPUT};
+            use super::super::super::{
+                fill_grid_part1, find_bounding_box, new_grid, BoundingBox, Cell,
+            };
+            use crate::{parse_input, tests::init_logger};
+            use ndarray::arr2;
+
+            #[test]
+            fn bounding_box() {
+                init_logger();
+                let input = "1, 1
+                             1, 6
+                             8, 3
+                             3, 4
+                             5, 5
+                             8, 9";
+                let parsed = parse_input(&input).unwrap();
+                let bounding_box = find_bounding_box(&parsed).unwrap();
+                assert_eq!(
+                    bounding_box,
+                    BoundingBox {
+                        xmin: 1,
+                        xmax: 8,
+                        ymin: 1,
+                        ymax: 9
+                    }
+                );
+            }
+
+            #[test]
+            fn grid_filling() {
+                init_logger();
+                let input = "1, 1
+                             1, 6
+                             8, 3
+                             3, 4
+                             5, 5
+                             8, 9";
+                let positions = parse_input(&input).unwrap();
+                let bounding_box = find_bounding_box(&positions).unwrap();
+                let (mut grid, grid_offset) = new_grid(&bounding_box);
+                for (id, pos) in positions.iter().enumerate() {
+                    grid[pos.ij(&grid_offset)] = Cell::C(id as u32);
+                }
+                let expected_initial = arr2(&[
+                    [
+                        Cell::C(0),
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::C(2),
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::C(3),
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::C(4),
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::C(1),
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                    ],
+                    [
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::U___,
+                        Cell::C(5),
+                    ],
+                ]);
+                assert_eq!(grid, expected_initial);
+                fill_grid_part1(&mut grid, &bounding_box, &positions);
+                let expected_filed = arr2(&[
+                    [
+                        Cell::C(0),
+                        Cell::C(0),
+                        Cell::C(0),
+                        Cell::C(0),
+                        Cell::T___,
+                        Cell::C(2),
+                        Cell::C(2),
+                        Cell::C(2),
+                    ],
+                    [
+                        Cell::C(0),
+                        Cell::C(0),
+                        Cell::C(3),
+                        Cell::C(3),
+                        Cell::C(4),
+                        Cell::C(2),
+                        Cell::C(2),
+                        Cell::C(2),
+                    ],
+                    [
+                        Cell::C(0),
+                        Cell::C(3),
+                        Cell::C(3),
+                        Cell::C(3),
+                        Cell::C(4),
+                        Cell::C(2),
+                        Cell::C(2),
+                        Cell::C(2),
+                    ],
+                    [
+                        Cell::T___,
+                        Cell::C(3),
+                        Cell::C(3),
+                        Cell::C(3),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(2),
+                        Cell::C(2),
+                    ],
+                    [
+                        Cell::C(1),
+                        Cell::T___,
+                        Cell::C(3),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(2),
+                    ],
+                    [
+                        Cell::C(1),
+                        Cell::C(1),
+                        Cell::T___,
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::T___,
+                    ],
+                    [
+                        Cell::C(1),
+                        Cell::C(1),
+                        Cell::T___,
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(5),
+                        Cell::C(5),
+                    ],
+                    [
+                        Cell::C(1),
+                        Cell::C(1),
+                        Cell::T___,
+                        Cell::C(4),
+                        Cell::C(4),
+                        Cell::C(5),
+                        Cell::C(5),
+                        Cell::C(5),
+                    ],
+                    [
+                        Cell::C(1),
+                        Cell::C(1),
+                        Cell::T___,
+                        Cell::C(5),
+                        Cell::C(5),
+                        Cell::C(5),
+                        Cell::C(5),
+                        Cell::C(5),
+                    ],
+                ]);
+                assert_eq!(grid, expected_filed);
+            }
         }
-        */
     }
 
     mod part2 {
@@ -79,9 +526,7 @@ mod tests {
             fn solution() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
+                let expected = 45602;
                 let to_check = Day06Initial::new(PUZZLE_INPUT).solution_part2();
 
                 assert_eq!(to_check, expected);
@@ -89,18 +534,24 @@ mod tests {
         }
 
         mod given {
-            use super::super::super::Day06Initial;
-            use crate::{tests::init_logger, AoC};
+            // use super::super::super::Day06Initial;
+            use super::super::super::solution_part2;
+
+            use crate::tests::init_logger;
 
             #[test]
             fn ex01() {
                 init_logger();
 
-                unimplemented!();
-
-                let expected = 0;
-                let input = "";
-                let to_check = Day06Initial::new(input).solution_part2();
+                let expected = 16;
+                let input = "1, 1
+                             1, 6
+                             8, 3
+                             3, 4
+                             5, 5
+                             8, 9";
+                // let to_check = Day06Initial::new(input).solution_part2();
+                let to_check = solution_part2(input, 32).unwrap();
 
                 assert_eq!(to_check, expected);
             }
@@ -112,5 +563,6 @@ mod tests {
             use crate::{tests::init_logger, AoC, PUZZLE_INPUT};
         }
         */
+
     }
 }
--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,4 +1,4 @@
-//! # Day 06:
+//! # Day 06: Chronal Coordinates
 //!
 //! [Benchmarking report](../../../day06/target/criterion/report/index.html):
 //!
@@ -8,17 +8,141 @@
 //!
 //! ## Part One
 //!
+//! The device on your wrist beeps several times, and once again you feel like you're falling.
 //!
+//! "Situation critical," the device announces. "Destination indeterminate. Chronal
+//! interference detected. Please specify new target coordinates."
+//!
+//! The device then produces a list of coordinates (your puzzle input). Are they places it
+//! thinks are safe or dangerous? It recommends you check manual page 729. The Elves did
+//! not give you a manual.
+//!
+//! _If they're dangerous,_ maybe you can minimize the danger by finding the coordinate
+//! that gives the largest distance from the other points.
+//!
+//! Using only the [Manhattan distance](https://en.wikipedia.org/wiki/Taxicab_geometry),
+//! determine the _area_ around each coordinate by counting the number of
+//! [integer](https://en.wikipedia.org/wiki/Integer) X,Y locations that are _closest_
+//! to that coordinate (and aren't _tied in distance_ to any other coordinate).
+//!
+//! Your goal is to find the size of the _largest area_ that isn't infinite. For
+//! example, consider the following list of coordinates:
+//!
+//! ```text
+//! 1, 1
+//! 1, 6
+//! 8, 3
+//! 3, 4
+//! 5, 5
+//! 8, 9
+//! ```
+//!
+//! If we name these coordinates `A` through `F`, we can draw them on a grid,
+//! putting `0,0` at the top left:
+//!
+//! ```text
+//! ..........
+//! .A........
+//! ..........
+//! ........C.
+//! ...D......
+//! .....E....
+//! .B........
+//! ..........
+//! ..........
+//! ........F.
+//! ```
+//!
+//! This view is partial - the actual grid extends infinitely in all directions.
+//! Using the Manhattan distance, each location's closest coordinate can be determined,
+//! shown here in lowercase:
+//!
+//! ```text
+//! aaaaa.cccc
+//! aAaaa.cccc
+//! aaaddecccc
+//! aadddeccCc
+//! ..dDdeeccc
+//! bb.deEeecc
+//! bBb.eeee..
+//! bbb.eeefff
+//! bbb.eeffff
+//! bbb.ffffFf
+//! ```
+//!
+//! Locations shown as `.` are equally far from two or more coordinates, and so they
+//! don't count as being closest to any.
+//!
+//! In this example, the areas of coordinates A, B, C, and F are infinite - while not
+//! shown here, their areas extend forever outside the visible grid. However, the
+//! areas of coordinates D and E are finite: D is closest to 9 locations, and E is
+//! closest to 17 (both including the coordinate's location itself). Therefore, in
+//! this example, the size of the largest area is _17_.
+//!
+//! _What is the size of the largest area_ that isn't infinite?
 //!
 //! ## Part Two
 //!
+//! On the other hand, _if the coordinates are safe_, maybe the best you can do is try
+//! to find a _region_ near as many coordinates as possible.
 //!
+//! For example, suppose you want the sum of the [Manhattan distance](https://en.wikipedia.org/wiki/Taxicab_geometry) to all of the coordinates to be
+//! _less than 32_. For each location, add up the distances to all of the given
+//! coordinates; if the total of those distances is less than 32, that location is
+//! within the desired region. Using the same coordinates as above, the resulting
+//! region looks like this:
+//!
+//! ```text
+//! ..........
+//! .A........
+//! ..........
+//! ...#$#..C.
+//! ..#D###...
+//! ..###E#...
+//! .B.###....
+//! ..........
+//! ..........
+//! ........F.
+//! ```
+//!
+//! In particular, consider the highlighted location `4,3` located at the top middle
+//! of the region. Its calculation is as follows, where `abs()` is the
+//! [absolute value](https://en.wikipedia.org/wiki/Absolute_value) function:
+//!
+//! * Distance to coordinate A: `abs(4-1) + abs(3-1) =  5`
+//! * Distance to coordinate B: `abs(4-1) + abs(3-6) =  6`
+//! * Distance to coordinate C: `abs(4-8) + abs(3-3) =  4`
+//! * Distance to coordinate D: `abs(4-3) + abs(3-4) =  2`
+//! * Distance to coordinate E: `abs(4-5) + abs(3-5) =  3`
+//! * Distance to coordinate F: `abs(4-8) + abs(3-9) = 10`
+//! * Total distance: `5 + 6 + 4 + 2 + 3 + 10 = 30`
+//!
+//! Because the total distance to all coordinates (`30`) is less than 32, the location
+//! is _within_ the region.
+//!
+//! This region, which also includes coordinates D and E, has a total size of _16_.
+//!
+//! Your actual region will need to be much larger than this example, though, instead
+//! including all locations with a total distance of less than _10000_.
+//!
+//! _What is the size of the region containing all locations which have a total
+//! distance to all given coordinates of less than 10000?_
 //!
 
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
 // #[macro_use]
 // extern crate log;
+extern crate itertools;
+#[macro_use]
+extern crate ndarray;
 
 use std::fmt::Debug;
+
+use std::error::Error;
+use std::result;
+use std::str::FromStr;
 
 pub mod initial;
 pub use initial::Day06Initial;
@@ -47,9 +171,51 @@ pub trait AoC<'a>: Debug {
     }
 }
 
-fn parse_input<'a>(input: &'a str) -> impl Iterator<Item = i64> + 'a {
-    unimplemented!();
-    vec![].into_iter()
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Position {
+    x: i64,
+    y: i64,
+}
+
+impl Position {
+    pub fn ij(&self, grid_offset: &[i64; 2]) -> [usize; 2] {
+        [
+            (self.y - grid_offset[1]) as usize,
+            (self.x - grid_offset[0]) as usize,
+        ]
+    }
+}
+
+type Result<T> = result::Result<T, Box<Error>>;
+
+impl FromStr for Position {
+    type Err = Box<Error>;
+
+    fn from_str(s: &str) -> Result<Position> {
+        let mut pos_str = s.split(", ");
+
+        Ok(Position {
+            x: pos_str
+                .next()
+                .ok_or_else(|| format!("failed to get first element of '{}'", s))?
+                .parse()?,
+            y: pos_str
+                .next()
+                .ok_or_else(|| format!("failed to get second element of '{}'", s))?
+                .parse()?,
+        })
+    }
+}
+
+pub fn parse_input(input: &str) -> Result<Vec<Position>> {
+    input.lines().map(|line| line.trim().parse()).collect()
+}
+
+pub fn manhattan_distance(pos1: &Position, pos2: &Position) -> i64 {
+    let delta_x = (pos1.x - pos2.x).abs();
+    let delta_y = (pos1.y - pos2.y).abs();
+
+    delta_x + delta_y
 }
 
 pub static PUZZLE_INPUT: &str = include_str!("../input");
@@ -65,10 +231,7 @@ pub mod benchmark {
     >;
 
     pub fn to_benchmark<'a>() -> BenchmarkVector<'a> {
-        unimplemented!()
-        // vec![
-        //     Box::new(Day06Initial::new(PUZZLE_INPUT)),
-        // ]
+        vec![Box::new(Day06Initial::new(PUZZLE_INPUT))]
     }
 }
 
@@ -77,7 +240,7 @@ mod tests {
     extern crate env_logger;
     use std::env;
 
-    use crate::parse_input;
+    use crate::{manhattan_distance, parse_input, Position};
 
     pub fn init_logger() {
         env::var("RUST_LOG")
@@ -87,14 +250,45 @@ mod tests {
                 println!("Setting to: {}", rust_log);
                 env::set_var("RUST_LOG", &rust_log);
                 Ok(rust_log)
-            }).unwrap();
+            })
+            .unwrap();
         let _ = env_logger::try_init();
     }
 
     #[test]
     fn parse() {
-        unimplemented!();
-        let parsed: Vec<_> = parse_input("").collect();
-        assert_eq!(parsed, vec![]);
+        assert_eq!("1, 1".parse::<Position>().unwrap(), Position { x: 1, y: 1 });
+        assert_eq!("1, 2".parse::<Position>().unwrap(), Position { x: 1, y: 2 });
+        assert_eq!("2, 1".parse::<Position>().unwrap(), Position { x: 2, y: 1 });
+
+        let input = "1, 1
+                     1, 6
+                     8, 3
+                     3, 4
+                     5, 5
+                     8, 9";
+        assert_eq!(
+            parse_input(&input).unwrap(),
+            vec![
+                Position { x: 1, y: 1 },
+                Position { x: 1, y: 6 },
+                Position { x: 8, y: 3 },
+                Position { x: 3, y: 4 },
+                Position { x: 5, y: 5 },
+                Position { x: 8, y: 9 },
+            ]
+        )
+    }
+
+    #[test]
+    fn calculate_manhattan_distance() {
+        assert_eq!(
+            manhattan_distance(&Position { x: 1, y: 1 }, &Position { x: 1, y: 6 }),
+            5
+        );
+        assert_eq!(
+            manhattan_distance(&Position { x: 1, y: 1 }, &Position { x: 8, y: 3 }),
+            9
+        );
     }
 }
